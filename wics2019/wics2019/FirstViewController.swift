c6efb588e51b2cb5dbb56a4f494b52522353e09b
Simran Sohal <simransohal@wireless-10-147-90-27.public.utexas.edu>
2019-02-09 12:45:51 -0600

    progress bar to home page

--- wics2019/wics2019/FirstViewController.swift
+++ wics2019/wics2019/FirstViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    let progressShape = CAShapeLayer()
+    let backgroundShape = CAShapeLayer()
+    let percent = 73.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.layer.addSublayer(backgroundShape)
+        view.layer.addSublayer(progressShape)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func updateIndicator(with percent: Double, isAnimated: Bool = false){
+        let animation = CABasicAnimation(keyPath: "strokeStart")
+        animation.fromValue = progressShape.strokeStart
+        animation.toValue = percent / 100.0
+        animation.duration = 2.5
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut);
+        
+        let shortestSide = min(view.frame.size.width - 75, view.frame.size.height - 75)
+        let strokeWidth: CGFloat = 60.0
+        let frame = CGRect(x: 0, y: -40, width: shortestSide - strokeWidth, height: shortestSide - strokeWidth)
+        
+        backgroundShape.frame = frame
+        backgroundShape.position = view.center
+        backgroundShape.path = UIBezierPath(ovalIn: frame).cgPath
+        backgroundShape.strokeColor = UIColor.black.cgColor
+        backgroundShape.lineWidth = strokeWidth
+        backgroundShape.fillColor = UIColor.clear.cgColor
+        
+        progressShape.frame = frame
+        progressShape.path = backgroundShape.path
+        progressShape.position = backgroundShape.position
+        progressShape.strokeColor = UIColor.red.cgColor
+        progressShape.lineWidth = backgroundShape.lineWidth
+        progressShape.fillColor = UIColor.clear.cgColor
+        progressShape.strokeEnd = CGFloat(percent/100.0)
+        
+        if isAnimated {
+            progressShape.add(animation, forKey: nil)
+        }
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateIndicator(with: percent, isAnimated: true)
+    }
+        
 }
+
+
+
 
